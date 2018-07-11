19a8d443c1c06a8d986a86c9c7d69ecb946d62c4
Влад Герасичкин <vladgerasickin@macbook-pro-vlad.local>
2018-07-11 21:06:44 +0300

    Initial Commit

---
+++ g64l2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  g64l2
+//
+//  Created by Влад Герасичкин on 09.07.2018.
+//  Copyright © 2018 Влад Герасичкин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
