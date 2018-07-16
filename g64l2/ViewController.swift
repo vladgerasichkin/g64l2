d47401f54e7bb8ac547847a9415035215ad2e7f7
Влад Герасичкин <vladgerasickin@macbook-pro-vlad.local>
2018-07-16 11:11:11 +0300

    Added Block 1 Task 0

--- g64l2/ViewController.swift
+++ g64l2/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  g64l2
 //
-//  Created by Влад Герасичкин on 09.07.2018.
+//  Created by Влад Герасичкин on 16.07.2018.
 //  Copyright © 2018 Влад Герасичкин. All rights reserved.
 //
 
@@ -12,17 +12,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        chooseLargerNumber(2, 5)
         
     }
-    
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    /////////////////Блок 1 Задача 0
+    func chooseLargerNumber(_ number1: Int, _ number2: Int ) {
+        if number1 > number2 {
+            print("Наибольшее из двух чисел -", number1)
+        }
+        else if number1 < number2 {
+            print("Наибольшее из двух чисел -", number2)
+        }
+        else {
+            print("Числа равны между собой и равняются -", number1)
+        }
     }
     
     
 }
+
 
